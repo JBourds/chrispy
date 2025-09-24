4390f1cf4cf773e523294585c3895538fc6b71de
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-09-24 14:35:18 -0400

    adc: Updating interrupt struct.

--- src/Adc.cpp
+++ src/Adc.cpp
@@ -32,81 +32,22 @@
 #define DIV_2 0b001
 #define DIV_2_2 0b000
 
-// static struct AdcFrame {
-//     // Flags field containing potential errors or information about active
-//     volatile uint8_t eflags;
-//     // Bit resolution to determine whether samples must be one or two bytes
-//     BitResolution res;
-//     // Maximum allowed channel index. Stored as `nchannels` - 1 to get rid of
-//     // a subtraction in the ISR
-//     uint8_t max_ch_index;
-//     // Current channel being recorded on. Alternates each sample
-//     // if `nchannels` > 1.
-//     volatile uint8_t ch_index;
-//     // Array of channel definitions
-//     Channel* channels;
-//     // Maximum allowed buffer index. Stored like this to save a subtraction.
-//     size_t max_buf_index;
-//     // Index into the current buffer
-//     volatile size_t sample_index;
-//     // Double buffer which gets swapped once sample_index would exceed `sz`
-//     // Guaranteed to
-//     uint8_t* buf1;
-//     uint8_t* buf2;
-//     // Buffer actively being written to. Only gets swapped when capacity is
-//     hit.volatile uint8_t* current;
-//     // Total counter of the number of samples collected
-//     volatile uint32_t collected;
-// } FRAME;
-//
-// ISR(ADC_vect) {
-//     if ((FRAME.eflags & EFULL) == EFULL) {
-//         return;
-//     }
-//
-//     // Only sample high byte if we are in > 8-bit resolution
-//     if (FRAME.res != BitResolution::Eight) {
-//         FRAME.current[FRAME.sample_index++] = ADCL;
-//     }
-//     FRAME.current[FRAME.sample_index] = ADCH;
-//
-//     // Hit the end of this buffer.
-//     // Because we didn't early return at the beginning, we know the other
-//     buffer
-//         // is available. Mark in `eflags` this buffer is ready to write to
-//         then swap
-//         // the buffers.
-//         if (FRAME.sample_index == FRAME.max_buf_index) {
-//         FRAME.sample_index = 0;
-//         bool use_buf_1 = !(FRAME.eflags & BUF1FULL);
-//         FRAME.eflags |= use_buf_1 ? BUF1FULL : BUF2FULL;
-//         FRAME.current = use_buf_1 ? FRAME.buf1 : FRAME.buf2;
-//     }
-//     else {
-//         ++FRAME.sample_index;
-//     }
-//
-//     // Wrapping channel index
-//     // FRAME.ch_index =
-//     //     FRAME.ch_index == FRAME.max_ch_index ? 0 : FRAME.ch_index + 1;
-//     // if (!activate(FRAME.channels[FRAME.ch_index])) {
-//     //     FRAME.eflags |= ECHANNEL;
-//     //     return;
-//     // }
-//     //
-//     FRAME.collected++;
-// }
-
 static struct AdcFrame {
     // Flags used to communicate state of sampling
     volatile uint8_t eflags;
     // Bit resolution being used for sampling
     BitResolution res;
+    // Array of channels being swapped betwee.
+    Channel* channels;
+    // Length of `channels` array
+    size_t nchannels;
+    // Current channel index
+    volatile size_t ch_index;
     // Double buffers being swapped between
     uint8_t* buf1;
     uint8_t* buf2;
     // Number of bytes in each buffer (guaranteed to be an increment of the
-    // number of bytes needed per sample).
+    // number of bytes needed per sample)
     size_t buf_sz;
     // Current sample index in the active buffer
     volatile size_t sample_index;
@@ -120,11 +61,52 @@ static int8_t init_frame(BitResolution res, uint8_t nchannels,
     // Slice up the buffer into a double buffer
     size_t bytes_per_sample = res == BitResolution::Eight ? 1 : 2;
     size_t samples_per_buf = sz / (2 * bytes_per_sample);
+    FRAME.channels = channels;
+    FRAME.nchannels = nchannels;
     FRAME.res = res;
     FRAME.buf_sz = samples_per_buf * bytes_per_sample;
     FRAME.buf1 = buf;
     FRAME.buf2 = buf + FRAME.buf_sz;
     return 0;
+}
+
+static inline bool activate(Channel& ch) {
+    int8_t mask = ch.mux_mask();
+    if (mask < 0) {
+        return false;
+    }
+    ADMUX &= ~MUX_MASK;
+    ADMUX |= mask;
+    // If the mask comes from a higher channel (>7) set MUX5
+    if (mask & HIGH_CHANNEL_MASK) {
+        ADCSRB |= (1 << MUX5);
+    } else {
+        ADCSRB &= ~(1 << MUX5);
+    }
+    return true;
+}
+
+int8_t Channel::mux_mask() {
+    switch (pin) {
+        case A0:
+            return 0b000;
+        case A1:
+            return 0b001;
+        case A2:
+            return 0b010;
+        case A3:
+            return 0b011;
+        case A4:
+            return 0b100;
+        case A5:
+            return 0b101;
+        case A6:
+            return 0b110;
+        case A7:
+            return 0b111;
+        default:
+            return -1;
+    }
 }
 
 ISR(ADC_vect) {
@@ -164,7 +146,7 @@ void Adc::disable_autotrigger() { ADCSRA &= ~(1 << ADATE); }
 
 void Adc::set_frequency(uint32_t sample_rate) {
     ADCSRA &= ~PRESCALER_MASK;
-    ADCSRA |= DIV_16;
+    ADCSRA |= DIV_64;
 }
 
 int8_t Adc::start(BitResolution res, uint32_t sample_rate) {
