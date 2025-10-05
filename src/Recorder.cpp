aca16cf98e39267951bcfacf405a76d50a194666
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-10-05 15:02:03 -0400

    feat (recording): Recorder class implementation for performing multi-channel sampling and reading out to files.

---
+++ src/Recorder.cpp
@@ -0,0 +1,83 @@
+#include "Recorder.h"
+
+#include <Arduino.h>
+
+#include "SdFunctions.cpp"
+
+const size_t MAX_CHANNEL_COUNT = 16;
+
+int64_t Recorder::record(const char *filenames[], BitResolution res,
+                         uint32_t sample_rate, uint32_t duration_ms,
+                         uint8_t *buf, size_t sz) {
+    if (sd == nullptr) {
+        return -1;
+    } else if (this->nchannels > MAX_CHANNEL_COUNT) {
+        return -2;
+    }
+
+    // This number is bounded by `MAX_CHANNEL_COUNT` so the VLA is alright
+    SdFile files[nchannels];
+    // Create files and write blank headers
+    WavHeader hdr;
+    for (size_t i = 0; i < nchannels; ++i) {
+        if (!(files[i].open(filenames[i], O_TRUNC | O_WRITE | O_CREAT) &&
+              files[i].write(&hdr, sizeof(hdr)) == sizeof(hdr))) {
+            close_all(files, nchannels);
+            return -3;
+        }
+    }
+
+    Adc adc(nchannels, channels, buf, sz);
+    uint8_t *tmp_buf = nullptr;
+    size_t tmp_sz = 0;
+    size_t ch_index = 0;
+    if (adc.start(res, sample_rate) != 0) {
+        return -4;
+    }
+    uint32_t deadline = millis() + duration_ms;
+    while (millis() <= deadline) {
+        if (adc.swap_buffer(&tmp_buf, tmp_sz, ch_index) == 0) {
+            if (tmp_buf == nullptr) {
+                continue;
+            }
+            size_t nwritten = files[ch_index].write(tmp_buf, tmp_sz);
+            if (nwritten != tmp_sz) {
+                adc.stop();
+                close_all(files, nchannels);
+                return -5;
+            }
+        }
+    }
+    uint32_t ncollected = adc.stop();
+    while (adc.drain_buffer(&tmp_buf, tmp_sz, ch_index) == 0) {
+        if (tmp_buf == nullptr) {
+            continue;
+        }
+        size_t nwritten = files[ch_index].write(tmp_buf, tmp_sz);
+        if (nwritten != tmp_sz) {
+            close_all(files, nchannels);
+            return -6;
+        }
+    }
+
+    // Make all files the exact same size then write out WAV header
+    int64_t rc = truncate_to_smallest(files, nchannels);
+    if (rc < 0) {
+        close_all(files, nchannels);
+        return -7;
+    }
+    uint32_t file_size = static_cast<uint32_t>(rc);
+    uint32_t per_ch_sample_rate =
+        ncollected / (nchannels * duration_ms / 1000.0);
+    hdr.fill(res, file_size, per_ch_sample_rate);
+    for (size_t i = 0; i < nchannels; ++i) {
+        if (!(files[i].seekSet(0) &&
+              files[i].write(&hdr, sizeof(hdr)) == sizeof(hdr))) {
+            close_all(files, nchannels);
+            return -8;
+        }
+    }
+
+    close_all(files, nchannels);
+    return 0;
+}
---
+++ src/Recorder.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "Adc.h"
+
+// TODO: Conditional compilation with SD library
+#include "SdFat.h"
+
+struct Recorder {
+    uint8_t nchannels;
+    Channel *channels;
+    SdFat *sd;
+
+    /**
+     * Constructor.
+     *
+     * @param nchannels: Number of channels to expect in `mic_pins` and
+     * `power_pins`.
+     * @param channels: Array of channels to record on.
+     * @param sd: SD card to use for recording.
+     */
+    Recorder(uint8_t _nchannels, Channel *_channels, SdFat *_sd)
+        : nchannels(_nchannels), channels(_channels), sd(_sd) {}
+
+    /**
+     * Uses the SD singleton to record to every file in `files` with the same
+     * sample rate and duration. Truncates all files to be equal to the shortest
+     * lengths.
+     *
+     * Invariants:
+     *  - SD is initialized and in the directory recordings should go.
+     *  - `files` is of at least `channels` length and contains valid filenames.
+     *
+     * @param filenames: Array of filenames to record to. Must be at least as
+     * long as `nchannels`.
+     * @param res: Bit resolution to record at.
+     * @param sample_rate: Requested sample rate for each channel.
+     * @param duration_ms: Length in milliseconds to record for.
+     * @param buf: Buffer allocated to receive ADC samples.
+     * @param sz: Buffer size.
+     *
+     * @returns (int32_t): The file size in bytes if successful. Returns a
+     * negative value if there is an error.
+     */
+    int64_t record(const char *filenames[], BitResolution res,
+                   uint32_t sample_rate, uint32_t duration_ms, uint8_t *buf,
+                   size_t sz);
+};
